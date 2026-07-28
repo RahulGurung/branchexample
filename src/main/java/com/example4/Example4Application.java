809224ffaf5d438ef57e2d49abfe0fe2c83aff77
RahulGurung <46669104+RahulGurung@users.noreply.github.com>
2026-07-28 06:07:08 +0530

    Update Example4Application.java

--- src/main/java/com/example4/Example4Application.java
+++ src/main/java/com/example4/Example4Application.java
@@ -8,7 +8,7 @@ public class Example4Application {
 
 	public static void main(String[] args)
 	{
-		int i=100;
+		int y=100;
 		System.out.println("login-otp developed!");
 		SpringApplication.run(Example4Application.class, args);
 	}
